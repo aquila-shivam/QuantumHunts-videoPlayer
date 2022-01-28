31cab8728c6a415ea906794c99ee689adc3c5302
aquila-shivam <shivam.52.nishad@gmail.com>
2022-01-28 12:41:01 +0000

    done

--- video.js
+++ video.js
@@ -1,6 +1,6 @@
 var videoSource = new Array();
 videoSource[0] = './video/trailer.mp4';
-videoSource[1] = './video/oceans.mp4';
+Addvideo = {key:1, dir:'./video/ad_videos/KFC Rockin Commercial 10 Sec.mp4'};
 let key = 0; // global
 const videoCount = videoSource.length;
 const element = document.getElementById("videos");
@@ -12,14 +12,14 @@ function playVideo(videoNum) {
     element.load();
     element.play();
 }
-document.getElementById('videos').addEventListener('ended', myFunctionHandle, false);
- 
-function myFunctionHandle() {
-    key++;
-    if (key == videoCount) {
-        // key = 0;
-        playVideo(key);
-    } else {
-        playVideo(key);
-    }
-}
+function add()
+ {
+     element.setAttribute("src", Addvideo.dir);
+     element.setAttribute("type",'video/mp4');
+     element.load();
+     element.play();
+     console.log("working");
+    
+ }
+
+ const ad=setTimeout((add),30000);
